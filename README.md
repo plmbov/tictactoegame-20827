20bd2e213f1aaf13c8ae28b16fe05684e9c3e839
plmbov <mateusz.bojarski90@gmail.com>
2020-02-28 00:05:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+##TicTacToe Game
+
+Simple Tic Tac Toe game built with use of HTML, CSS and JS.
---
+++ script.js
@@ -0,0 +1,108 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const endGameMessage = document.getElementById('endGameMessage')
+let endGameMessageText = document.querySelector('[data-end-game-message-text]')
+const restartButton = document.getElementById('restartButton')
+const setNamesWindow = document.getElementById('setNamesWindow')
+const backdrop = document.getElementById('backdrop')
+const namesForm = document.getElementById('namesForm')
+const setNamesButton = document.getElementById('setNames')
+const playerOneNameDisplay = document.getElementById('playerOneNameDisplay')
+const playerTwoNameDisplay = document.getElementById('playerTwoNameDisplay')
+const changeNamesButton = document.getElementById('changeNamesButton')
+
+let circleTurn = Math.random() >= 0.5
+
+setNamesButton.addEventListener('click', setPlayersNames)
+namesForm.addEventListener('on-submit', setPlayersNames)
+restartButton.addEventListener('click', startGame)
+changeNamesButton.addEventListener('click', () => {
+    setNamesWindow.classList.add('show')
+    backdrop.classList.add('show')
+})
+backdrop.addEventListener('click', () => {
+    setNamesWindow.classList.remove('show')
+    backdrop.classList.remove('show')
+})
+
+
+setNamesWindow.classList.add('show')
+startGame()
+
+function setPlayersNames(event) {
+    event.preventDefault()
+    playerOneNameDisplay.innerText = document.getElementById('playerOneName').value
+    playerTwoNameDisplay.innerText = document.getElementById('playerTwoName').value
+    setNamesWindow.classList.remove('show')
+    backdrop.classList.remove('show')
+}
+
+function startGame() {
+    cellElements.forEach(el => {
+        el.classList.remove(X_CLASS)
+        el.classList.remove(CIRCLE_CLASS)
+        el.removeEventListener('click', handleClick)
+        el.addEventListener('click', handleClick, { once: true })
+    })
+    setBoardHoverClass()
+    endGameMessage.classList.remove('show')
+}
+
+function handleClick(event) {
+    const cell = event.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+    if (checkWin(currentClass)) {
+        endGameMessageText.innerHTML = `${circleTurn ? `${document.getElementById('playerOneName').value}` : `${document.getElementById('playerTwoName').value}`} wins!`
+        endGameMessage.classList.add('show')
+    } else if (isDraw()) {
+        endGameMessageText.innerHTML = `Draw`
+        endGameMessage.classList.add('show')
+    }
+    switchPlayer()
+    setBoardHoverClass()
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+function switchPlayer() {
+    circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if (circleTurn) {
+        board.classList.add(CIRCLE_CLASS)
+    } else {
+        board.classList.add(X_CLASS)
+    }
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+    })
+}
